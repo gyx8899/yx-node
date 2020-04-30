c9d8c9f87bdea7933b7abf39c4add05c71f60701
Steper Kuo <gyx8899@126.com>
2020-04-30 10:47:46 +0800

    fix(index): Download compatibility for already using cases;

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-exports.file = require('./file');
+exports.yxFile = require('./file');
 
-exports.folder = require('./folder');
+exports.yxFolder = require('./folder');
 
-exports.webpackTool = require('./webpackTool');
+exports.yxWebpack = require('./webpack');
 
-exports.util = require('./util');
+exports.yxUtil = require('./util');
+
+// Below is just downward compatibility for "@daybyday/yx-node": "^0.1.4",
+const {readData, writeData} = require('./file');
+const {iterateObject, getNodeArgs} = require('./util');
+const {getDiffFileNames} = require('./folder');
+
+exports.readDataFromFile = readData;
+exports.writeDataToFile = writeData;
+exports.iterateObject = iterateObject;
+exports.getNodeArgs = getNodeArgs;
+exports.getDiffFolderFileNames = getDiffFileNames;
