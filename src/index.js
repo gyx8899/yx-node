2b05d159dadc44c47ac92c5ce5601c42dacaa406
Steper Kuo <gyx8899@126.com>
2020-04-30 12:44:53 +0800

    fix(index): Revert webpack functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 const net = require('net');
 const path = require('path');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const glob = require('glob');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // Utils
 function iterateObject(obj, level, callbackNonLeaf, callbackLeaf) {
@@ -179,62 +175,6 @@ function getFileFromDirectory(filePath, fileCallback, folderCallback) {
   });
 }
 
-// Webpack
-const setMPA = (entryDir = './src/*/index.js', entryRegexp = /src\/(.*)\/index\.js/) => {
-  const entry = {};
-  const htmlWebpackPlugins = [];
-  const entryFiles = glob.sync(path.join(__dirname, entryDir));
-
-  Object.keys(entryFiles)
-      .map((index) => {
-        const entryFile = entryFiles[index];
-        // '/Users/cpselvis/my-project/src/index/index.js'
-
-        const match = entryFile.match(entryRegexp);
-        const pageName = match && match[1];
-
-        entry[pageName] = entryFile;
-        htmlWebpackPlugins.push(
-            new HtmlWebpackPlugin({
-              template: path.join(__dirname, `src/${pageName}/index.html`),
-              filename: `${pageName}.html`,
-              chunks: [pageName],
-              inject: true,
-              minify: {
-                html5: true,
-                collapseWhitespace: true,
-                preserveLineBreaks: false,
-                minifyCSS: true,
-                minifyJS: true,
-                removeComments: false,
-              },
-            }),
-        );
-      });
-
-  return {
-    entry,
-    htmlWebpackPlugins,
-  };
-};
-
-const checkServerPort = (port = 3000, hostname = 'localhost') => {
-  const server = net.createServer().listen(port, hostname);
-  return new Promise((resolve) => {
-    server.on('listening', () => {
-      server.close();
-      resolve(port);
-    });
-    server.on('error', (err) => {
-      // eslint-disable-next-line no-console
-      console.log('Error: (checkServerPort) ', err);
-      // TODO: would you like open the app on another port instead
-      resolve(port + 1);
-    });
-  });
-};
-
-
 module.exports = {
   // Utils
   iterateObject,
@@ -256,8 +196,4 @@ module.exports = {
   getDiffFolderFileNames,
   getFileFromDirectory,
   readDirectory,
-
-  // Webpack
-  setMPA,
-  checkServerPort,
 };
